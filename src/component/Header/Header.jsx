469e7b7686ad2b25faf7afd57a80e21a49af00f1
Florent Zheng <florentzheng01@gmail.com>
2022-10-24 15:37:21 +0200

    feat : added header component

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,3 +1,18 @@
 import './header.css'
+import Logo from '../Logo/Logo';
 
-const Header = ({})
+const Header = ({logo}) => {
+    return (
+            <nav
+            className='header'
+            >
+            <ul>
+                <li>Page 1</li>
+                <li><Logo logo={logo}></Logo></li>
+                <li>Page 3</li>
+            </ul>
+            </nav>
+    );
+}
+
+export default Header;
---
+++ src/component/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Title from '../../../../../mon-app/src/components/Title/Title';
+
+function Home() {
+    return (
+        <Title>this is the homepage</Title>
+    );
+}
+
+export default Home;
