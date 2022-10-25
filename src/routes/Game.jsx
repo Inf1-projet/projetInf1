a16cb7b7bf7b1799502c4ffa60f7d5f04ee19ed4
Florent Zheng <florentzheng01@gmail.com>
2022-10-25 13:21:50 +0200

    feat: front game page

--- src/routes/Game.jsx
+++ src/routes/Game.jsx
@@ -1,11 +1,19 @@
-import Text from "../component/Text/Text";
+import Title from "../component/Title/Title"
 import { useEffect, useState } from "react";
 import Image from "../component/Image/Image";
 import Header from "../component/Header/Header";
+import './game.css'
+import Layout from "../component/Layout/Layout";
+import Text from "../component/Text/Text";
+
   function Game() {
   
     const [pokemonImage, setPokemonImage] = useState([]);
     const [pokemonName, setPokemonName] = useState([]);
+
+    const [score, setScore] = useState(0);;
+    
+
     const fetchPokemons = async () => {
       const randomNumber = Math.floor(Math.random() * 160);
       const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`);
@@ -22,9 +30,15 @@ import Header from "../component/Header/Header";
     return (
           <>
             <Header />
-            <div>
-            <Image taille="large" image={pokemonImage}/>
-            <Text>{pokemonName}</Text>
+            <div className="body">
+              <div className="top-body">
+              <Title children="Who's that Pokemon !" size="large"></Title>
+              <Text children={score} className="score"></Text>
+              </div>
+              <div className="pokemon">
+                <Image taille="large" image={pokemonImage}/>
+                <Layout textButton="Submit" labelInput="Guess the pokemon"></Layout>
+              </div>
             </div>
           </>
     );
