0a396205bcba9d265d05313134b22bc79f586483
Florent Zheng <florentzheng01@gmail.com>
2022-10-25 11:37:14 +0200

    feat : added image and name in Game page

--- src/routes/Game.jsx
+++ src/routes/Game.jsx
@@ -1,22 +1,34 @@
-import { useEffect } from "react";
-function Game() {
+import Text from "../component/Text/Text";
+import { useEffect, useState } from "react";
+import Image from "../component/Image/Image";
+import Header from "../component/Header/Header";
+  function Game() {
   
-  const fetchPokemons = async () => {
-
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/2`);
-    const pokemons = await response.json();
-
-    console.log(pokemons);
-
-  }
+    const [pokemonImage, setPokemonImage] = useState([]);
+    const [pokemonName, setPokemonName] = useState([]);
+    const fetchPokemons = async () => {
+      const randomNumber = Math.floor(Math.random() * 160);
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`);
+      const pokemonImage = await response.json();  
+      setPokemonImage(pokemonImage.sprites.front_default);
+      setPokemonName(pokemonImage.name);
   
-  useEffect(() => {
-    fetchPokemons();
-  }, [])
+    }
+    
+    useEffect(() => {
+      fetchPokemons();
+    }, [])
+    
+    return (
+          <>
+            <Header />
+            <div>
+            <Image taille="large" image={pokemonImage}/>
+            <Text>{pokemonName}</Text>
+            </div>
+          </>
+    );
+    }
   
-  return (
-    <h1>title</h1>
-  );
-  }
-
-export default Game;
+  export default Game;
+  
