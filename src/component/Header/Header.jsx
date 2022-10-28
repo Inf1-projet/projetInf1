00d4a94bb7b8799a0546ec61edf3513edcafebd9
Florent Zheng <florentzheng01@gmail.com>
2022-10-28 14:00:15 +0200

    using component

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,16 +1,16 @@
 import './header.css'
 import Logo from '../Logo/Logo';
 import { Link } from 'react-router-dom';
-
+import Li from '../Li/Li';
 const Header = () => {
     return (
         <nav
         className='header'
         >
         <ul>
-            <li><Link to="/" className='link'>Home</Link></li>
-            <li><Logo logo="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Pokebola-pokeball-png-0.png/800px-Pokebola-pokeball-png-0.png"></Logo></li>  
-            <li><Link to="/game" className='link'>Game</Link></li>
+            <Li item={<Link to="/" className='link'>Home</Link>}></Li>
+            <Li item={<Logo logo="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Pokebola-pokeball-png-0.png/800px-Pokebola-pokeball-png-0.png"></Logo>}></Li>  
+            <Li item={<Link to="/game" className='link'>Game</Link>}></Li>
         </ul>
         </nav>  );
 
